56cac97eab6f82f34b314c5b737e3c0f5ddc9143
Simon HSU <bsebadmin@gmail.com>
2014-07-26 12:25:10 +0800

    20140725 - init project of AppGuidePage

---
+++ assets/720x720/GuidePage.qml
@@ -0,0 +1,73 @@
+import bb.cascades 1.0
+
+Page {
+    property int currentLeftX
+    property int touchDownX
+    property int leftToRight
+    property int downX;
+    property double moveThreshold: 100
+    property int currentPage: 0
+    property int howManyPages: 5
+    property int displayWidth: 720
+    property int displayHeight: 720
+    
+    Container {
+        background: Color.Black
+        ScrollView {
+            id: scrollView
+            scrollViewProperties.scrollMode: ScrollMode.Horizontal
+            Container {
+                layout: DockLayout {
+
+                }
+                ImageView {
+                    preferredWidth: displayWidth * howManyPages
+                    preferredHeight: displayHeight
+                    imageSource: "asset:///images/allguides_q10_en.jpg"
+                }
+                Button {
+                    translationX: 3070
+                    translationY: 600
+                    id: closeBtn
+                    text: "Enter"
+                    onClicked: {
+                        nav.pop();
+
+                    }
+                }
+            }
+           
+            onTouch: {
+                if (event.isUp()) {
+                    if (leftToRight == 0) {
+                        if (currentPage+1 < howManyPages)
+                        {
+                            currentPage = currentPage+1;
+                            scrollView.scrollToPoint(displayWidth*currentPage,0,ScrollAnimation.Smooth);
+                        }
+                    } else if (leftToRight == 1) {
+                        if (currentPage-1 >=0)
+                        {
+                            currentPage = currentPage-1;
+                            scrollView.scrollToPoint(displayWidth*currentPage,0,ScrollAnimation.Smooth);
+                        }
+                    } else {
+                        scrollView.scrollToPoint(displayWidth*currentPage,0,ScrollAnimation.Smooth);
+                    }
+                    leftToRight = -1
+                } else if (event.isMove()) {
+                    if (event.windowX-downX < -moveThreshold) {
+                        leftToRight = 0
+                    } else if (event.windowX-downX > moveThreshold) {
+                        leftToRight = 1
+                    } else if (event.windowX-downX >= -moveThreshold && event.windowX-downX <= moveThreshold){
+                        leftToRight = -1;
+                    }
+                } else if (event.isDown()) {
+                    downX = event.windowX;
+                    touchDownX = event.windowX+currentPage*displayWidth
+                }
+            }
+        }
+    }
+}
---
+++ translations/AppGuidePage.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>GuidePage3</name>
+    <message>
+        <location filename="../assets/720x1280/GuidePage3.qml" line="35"/>
+        <location filename="../assets/720x720/GuidePage3.qml" line="35"/>
+        <location filename="../assets/GuidePage3.qml" line="35"/>
+        <source>Page A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/720x1280/GuidePage3.qml" line="52"/>
+        <location filename="../assets/720x720/GuidePage3.qml" line="52"/>
+        <location filename="../assets/GuidePage3.qml" line="52"/>
+        <source>Page B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/720x1280/GuidePage3.qml" line="69"/>
+        <location filename="../assets/720x720/GuidePage3.qml" line="69"/>
+        <location filename="../assets/GuidePage3.qml" line="69"/>
+        <source>Page C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/720x1280/GuidePage3.qml" line="86"/>
+        <location filename="../assets/720x720/GuidePage3.qml" line="86"/>
+        <location filename="../assets/GuidePage3.qml" line="86"/>
+        <source>Page D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/720x1280/GuidePage3.qml" line="103"/>
+        <location filename="../assets/720x720/GuidePage3.qml" line="103"/>
+        <location filename="../assets/GuidePage3.qml" line="103"/>
+        <source>Last Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="71"/>
+        <source>OneImage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="81"/>
+        <source>Image Segments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="91"/>
+        <source>Custom Segments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="23"/>
+        <source>Three Types of Guide Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="28"/>
+        <source>OneImage:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="34"/>
+        <source>A large image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="41"/>
+        <source>Images Segments:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="47"/>
+        <source>Several images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="54"/>
+        <source>Custom Segments:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="60"/>
+        <source>Custom containers</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
